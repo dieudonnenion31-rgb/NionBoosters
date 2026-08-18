c4b59f0a4abb5aaf438391e49a3282c177211f10
dieudonnenion31-rgb <dieudonnenion31@gmail.com>
2026-08-18 23:11:18 +0000

    Create GrowthRule.jsx

---
+++ src/components/GrowthRule.jsx
@@ -0,0 +1,18 @@
+export default function GrowthRule() {
+  return (
+    <svg
+      className="growth-rule"
+      viewBox="0 0 300 14"
+      preserveAspectRatio="none"
+      aria-hidden="true"
+    >
+      <polyline
+        points="0,13 40,13 40,10 90,10 90,7 150,7 150,4 220,4 220,1 300,1"
+        fill="none"
+        stroke="#e8a33d"
+        strokeWidth="2"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
